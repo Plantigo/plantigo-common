4a9d6557a1be5e2f429292593dae70c3b1da6a10
jaqb8 <jakubaniszewski@pm.me>
2024-11-13 21:28:34 +0100

    Bump version to 1.0.1 in pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="plantigo-common",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=["fastapi>=0.70.0", "grpcio>=1.39.0"],
     description="Reusable modules for Plantigo project",
