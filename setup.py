2dc735b8e5148eeb20549a7af0daddf897023801
jaqb8 <jakubaniszewski@pm.me>
2024-11-16 17:08:55 +0100

    Add python-jose dependency to pyproject.toml and setup.py for JWT handling

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ setup(
     name="plantigo-common",
     version="1.0.5",
     packages=find_packages(),
-    install_requires=["fastapi>=0.70.0", "grpcio>=1.39.0", "grpc-interceptor>=0.15.4"],
+    install_requires=[
+        "fastapi>=0.70.0",
+        "grpcio>=1.39.0",
+        "grpc-interceptor>=0.15.4" "python-jose>=3.3.0",
+    ],
     description="Reusable modules for Plantigo project",
     author="jakubaniszewski@pm.me",
     license="MIT",
