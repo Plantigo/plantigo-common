11c77175e74659424746b008ea4d9b2cc42acab3
jaqb8 <jakubaniszewski@pm.me>
2024-11-17 19:16:29 +0100

    1.0.7 - readme

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="plantigo-common",
-    version="1.0.7",
+    version="1.0.8",
     packages=find_packages(),
     install_requires=[
         "fastapi>=0.70.0",
@@ -13,6 +17,8 @@ setup(
         "protobuf>=5.28.3",
     ],
     description="Reusable modules for Plantigo project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="jakubaniszewski@pm.me",
     license="MIT",
     classifiers=[
